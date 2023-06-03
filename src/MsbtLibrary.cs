947c89e19b619e948624b15b44926b50b9c6aa87
ArchLeaders <archparliament@gmail.com>
2023-06-03 01:35:04 -0700

    Impl NativeLibrary

---
+++ src/MsbtLibrary.cs
@@ -0,0 +1,8 @@
+﻿using Native.IO;
+using Native.IO.Services;
+
+namespace CsMsbt;
+public class MsbtLibrary : NativeLibrary<MsbtLibrary>, INativeLibrary
+{
+    protected override string Name { get; } = "cs_msbt";
+}
