b78575618ef899134b46c6c2e24a404a4d8e0d6c
ArchLeaders <archparliament@gmail.com>
2023-06-03 01:35:18 -0700

    Impl managed wrapper

---
+++ src/Msbt.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32.SafeHandles;
+using Native.IO.Handles;
+using System.Runtime.InteropServices;
+
+namespace CsMsbt;
+
+public unsafe partial class Msbt : SafeHandleZeroOrMinusOneIsInvalid
+{
+    [LibraryImport("cs_restbl")]
+    private static partial Msbt FromBinary(byte* src, int src_len);
+
+    [LibraryImport("cs_restbl")]
+    private static partial DataMarshal ToBinary(Msbt handle);
+
+    [LibraryImport("cs_restbl", StringMarshalling = StringMarshalling.Utf8)]
+    private static partial Msbt FromText(string text);
+
+    [LibraryImport("cs_restbl")]
+    private static partial StringMarshal ToText(Msbt handle);
+
+    [LibraryImport("cs_restbl")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool Free(IntPtr handle);
+
+    public Msbt() : base(true) { }
+
+    public static Msbt FromBinary(ReadOnlySpan<byte> data)
+    {
+        fixed (byte* ptr = data) {
+            return FromBinary(ptr, data.Length);
+        }
+    }
+
+    public DataMarshal ToBinary()
+    {
+        return ToBinary(this);
+    }
+
+    public static Msbt FromText(ReadOnlySpan<byte> data)
+    {
+        fixed (byte* ptr = data) {
+            return FromBinary(ptr, data.Length);
+        }
+    }
+
+    public StringMarshal ToText()
+    {
+        return ToText(this);
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        return Free(handle);
+    }
+}
